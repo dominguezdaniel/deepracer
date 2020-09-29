e833320f3a438ceb08a4c072af95a2da398c2da6
Daniel Dominguez <dominguezdaniel@live.com>
2020-09-28 22:21:49 -0500

    Update Model#3.py

--- Model#3.py
+++ Model#3.py
@@ -1,19 +1,31 @@
 import math
+
 def reward_function(params):
-'''
-Use square root for center line
-'''
-track_width = params['track_width']
-distance_from_center = params['distance_from_center']
-steering = abs(params['steering_angle'])
-speed = params['speed']
-all_wheels_on_track = params['all_wheels_on_track']
-ABS_STEERING_THRESHOLD = 15
-reward = 1 - (distance_from_center / (track_width/2))**(4)
-if reward < 0:
-reward = 0
-if steering > ABS_STEERING_THRESHOLD:
-reward *= 0.8
-if not (all_wheels_on_track):
-reward = 0
-return float(reward)
+    '''
+    Use square root for center line
+    '''
+
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    speed = params['speed']
+    progress = params['progress']
+    all_wheels_on_track = params['all_wheels_on_track']
+   
+    SPEED_TRESHOLD = 2
+
+    reward = 1 - (distance_from_center / (track_width/2))**(4)
+
+    if reward < 0:
+        reward = 0
+
+    if speed > SPEED_TRESHOLD:
+        reward *= 0.8
+
+    if not (all_wheels_on_track):
+        reward = 0
+
+    if progress == 100:    
+        reward += 100
+
+    return float(reward)
