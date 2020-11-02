bde36a2680f0d3f742236b2a237cd28ed3648330
Daniel Dominguez <dominguezdaniel@live.com>
2020-11-02 18:25:02 -0500

    Create Model#5.py

---
+++ Model#5.py
@@ -0,0 +1,44 @@
+def reward_function(params):
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    all_wheels_on_track = params['all_wheels_on_track']
+    speed = params['speed']
+    SPEED_THRESHOLD = 1.0 
+    steering = abs(params['steering_angle']) # We don't care whether it is left or right steering
+
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    if not all_wheels_on_track:
+		# Penalize if the car goes off track
+        reward = 1e-3
+    elif speed < SPEED_THRESHOLD:
+		# Penalize if the car goes too slow
+        reward = reward + 0.5
+    else:
+		# High reward if the car stays on track and goes fast
+        reward = reward + 1.0
+        
+    
+    # Initialize the reward with typical value 
+    reward = 1.0
+
+    # Penalize if car steer too much to prevent zigzag
+    STEERING_THRESHOLD = 20.0
+    if steering > STEERING_THRESHOLD:
+        reward *= 0.8
+
+    return float(reward)
